57b9873dc5e4184c9e8b8b4bc7df8f4146513c0d
KRX <KRX@KRX-PC>
2017-07-03 14:39:20 +0900

    initial commit

---
+++ shbt-board/src/main/java/net/shbtboard/ShbtBoardApplication.java
@@ -0,0 +1,12 @@
+package net.shbtboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShbtBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShbtBoardApplication.class, args);
+	}
+}
