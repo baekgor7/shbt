fe0e70eb7ff55ec4e087304a1381bea3b80b22a6
KRX <KRX@KRX-PC>
2017-07-10 16:43:04 +0900

    login & logout

---
+++ shbt-board/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (USER_ID, PW, USER_NM, EMAIL) VALUES ('aaa', 'pwaaa', '홍길동', 'aa@aa.com');
+INSERT INTO USER (USER_ID, PW, USER_NM, EMAIL) VALUES ('bbb', 'pwbbb', '임꺽정', 'bb@bb.com');
